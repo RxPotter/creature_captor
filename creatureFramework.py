9b539df4c6877c05dfb284a187047f55df19b206
RxPotter <rxpotter@students.everettcc.edu>
2025-03-21 10:16:08 +0000

    Creature class defined

--- creatureFramework.py
+++ creatureFramework.py
@@ -1,4 +1,18 @@
-name = ""
-statLabels = ["HP","Attack","Defence","Psi Attack","Psi Defence","Speed"]
-statValues = [0,0,0,0,0,0]
 
+class Creature:
+    def __init__(self, name, types, moves, IVs, health='==================='):
+        # here we save variables as attributes
+        self.name = name
+        self.types = types
+        self.moves = moves
+        self.attack = IVs['ATTACK']
+        self.defence = IVs['DEFENCE']
+        self.bars = 20 # Set number of health bars
+
+
+    def fight(self, Creature2):
+        #this function allows two creature to fight eachother
+        #print fight info
+        print("-----CREATURE COMBAT-----")
+        print(f"\n{self.name}")
+        
