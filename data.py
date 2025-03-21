6c7af7ae8f1efbe7629ca7945939c9e6271ae2f5
RxPotter <rxpotter@students.everettcc.edu>
2025-03-21 17:51:49 +0000

    Added Data.py

---
+++ data.py
@@ -0,0 +1,7 @@
+CREATURE_DATA = {
+    'Blaze Adder': {
+        'stats': {'type': 'Heat','max_health': 12, 'attack': 12, 'defence': 8, 'regeneration': 1, 'speed': 15},
+        'moves': {'Scorch', 'Glide', 'Tackle', 'Bite'},
+        
+    }
+}
--- main.py
+++ main.py
@@ -8,7 +8,15 @@ def convertCSVto2Dlist(csvFile: str):
         for mapRow in csv.reader(file):
             tileMap.append(list(map(int, mapRow)))
     return tileMap
-mapData = convertCSVto2Dlist(csvFile = "map.csv")           
-player1 = Player("Gaia", mapData[14][0], mapData[0][0])
-   
+mapData = convertCSVto2Dlist(csvFile = "map.csv")     
+
+player1 = Player("Gaia", mapData[14][0], mapData[14][0])
+def checkPaths():
+    for i, x in enumerate(directions):
+        if mapData[player1.x + 1][player1.y] != 0:
+            print(f"{i+1}.", x)
+            #int(input('pick a direction: '))
+        else:
+            break
 print(player1)
+checkPaths()
