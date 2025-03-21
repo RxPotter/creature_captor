d092283239ae5dd8221cd5195e5579a26d2f82c0
RxPotter <rxpotter@students.everettcc.edu>
2025-03-21 15:46:41 +0000

    Checkpoint

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import csv
-import player
+from player import*
 
 directions = ['North', 'East', 'South', 'West']
-print()
-
-    
-                    
+def convertCSVto2Dlist(csvFile: str):
+    tileMap = []
+    with open(csvFile, 'r', newline='') as file:
+        for mapRow in csv.reader(file):
+            tileMap.append(list(map(int, mapRow)))
+    return tileMap
+mapData = convertCSVto2Dlist(csvFile = "map.csv")           
+player1 = Player("Gaia", mapData[14][0], mapData[0][0])
+   
+print(player1)
--- player.py
+++ player.py
@@ -1,8 +1,11 @@
 class Player:
-    def __init__(self, x ,y):
+    def __init__(self, name, x ,y):
+        self.name = name
         self.x = x
         self.y = y
 
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
+    def __str__(self):
+      return f"Name: {self.name}, X: {self.x}, Y: {self.y}"
